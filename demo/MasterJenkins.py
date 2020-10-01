11484f9c7fc097def88860c74e9ab909d49a7600
vaibhavgoel379 <54577802+vaibhavgoel379@users.noreply.github.com>
2020-10-01 15:41:46 +0530

    Update MasterJenkins.py
    hogya bass ab

--- demo/MasterJenkins.py
+++ demo/MasterJenkins.py
@@ -1,8 +1,9 @@
-import add
-import mul
-import Division
-import subtract
+from add import add
+from mul import mul
+from Division import div
+from subtract import subtract
 
-print(sub(4,2))
+add(4,3)
+print(subtract(4,2))
 print(div(4,2))
 print(mul(4,2))
