c52457a98bd79d990ebbb787f6f5dfdd78c86ca3
vaibhavgoel379 <goelvaibhav379@gmail.com>
2020-10-01 14:53:43 +0530

    Master

---
+++ demo/Master.py
@@ -0,0 +1,12 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+
+def div(a,b):
+	result=a//b
+	return result
+
+print("Output Add",add(a,b))
+print("Output Sub",sub(a,b))
+print("Output Mul",mul(a,b))
+print("Output Div",div(a,b))
+
